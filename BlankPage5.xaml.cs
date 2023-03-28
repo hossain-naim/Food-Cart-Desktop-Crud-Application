fd67c2e9b2685f674a5871e87a3e9c85e711da49
Hossain Naim <hmnaim4140@protonmail.com>
2023-03-28 12:45:59 +0600

    Add files via upload

---
+++ BlankPage5.xaml.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MonthlyProject
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BlankPage5 : Page
+    {
+        private void dowork(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(BlankPage1));
+
+        }
+
+        private void dowork2(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(BlankPage4));
+        }
+        private void dowork3(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+        public BlankPage5()
+        {
+            this.InitializeComponent();
+            //Item item;
+
+            //Uri _baseUri = new Uri("ms-appx:///");
+
+            //item = new Item();
+
+            //item.Title = "Acer Aspire S3-391 Laptop";
+
+            //item.Price = "Rs. 66,754";
+
+            //item.SetImage(_baseUri, "Images/wood.jpg");
+
+            //item.Content = "Core i7 (3G), 13.3 Inch4,  GB RAM, 500 GB HD, Win 7";
+
+
+
+            //item = new Item();
+
+            //item.Title = "Acer Aspire S3 Aspire S Laptop";
+
+            //item.Price = "Rs. 54,500";
+
+            //item.SetImage(_baseUri, "Images/2.jpg");
+
+            //item.Content = "Core i5 (2G), 13.3 Inch, 4 GB RAM, 320 GB HD, Win 7";
+
+            //Collection.Add(item);
+
+
+
+            //item = new Item();
+
+            //item.Title = "Acer Aspire One 725 Laptop";
+
+            //item.Price = "Rs. 19,499";
+
+            //item.SetImage(_baseUri, "Images/3.jpg");
+
+            //item.Content = "AMD APU Dual Core, 11.6 Inch, 2 GB RAM, 320 GB HD, Win 7";
+
+            //Collection.Add(item);
+
+
+
+            //item = new Item();
+
+            //item.Title = "Acer Aspire One AOD 270 NU.SGASI.003 Netbook";
+
+            //item.Price = "Rs. 16,560";
+
+            //item.SetImage(_baseUri, "Images/4.jpg");
+
+            //item.Content = "Atom Dual Core (2G), 10.1 Inch, 2 GB RAM, 320 GB HD, Linux";
+
+            //Collection.Add(item);
+
+
+
+            //item = new Item();
+
+            //item.Title = "Acer Aspire V3-571G Laptop";
+
+            //item.Price = "Rs. 38,455";
+
+            //item.SetImage(_baseUri, "Images/6.jpg");
+
+            //item.Content = "Core i3 (2G), 15.6 Inch, 4 GB RAM, 500 GB HD, Win 7";
+
+            //Collection.Add(item);
+            PopulateProjects();
+
+        }
+
+        private void PopulateProjects()
+        {
+            //List<Items> Projects = new List<Items>();
+            GetRecords();
+            
+            //Project2 newProject = new Project2();
+            //newProject.Activities.Add(new Items()
+            //{ ITEMCODE = "L-001", ITEMNAME = "LUX BIG", PURCHASEPRICE = 100,SALESPRICE=120,QTY=19, DEPT="SOAP",EXPIREDATE="2022-01-01",IMAGE= @"C:\Users\Sir  Pc\AppData\Local\Packages\a14801bb-9e14-4fa5-ac28-481fd580c4d2_4wfb1cc211pwr\LocalState\avatar.jpg" });
+            //newProject.Activities.Add(new Items()
+            //{ ITEMCODE = "L-002", ITEMNAME = "LUX MEDIUM", PURCHASEPRICE = 45, SALESPRICE = 60, QTY = 32, DEPT = "SOAP", EXPIREDATE = "2025-01-01" });
+           // Collection.Add(collection);
+
+
+
+            //cvsProjects.Source = Projects;
+        }
+        public async Task GetRecords()
+        {
+
+            var folder = ApplicationData.Current.LocalFolder;
+
+            try
+            {
+                var file = await folder.OpenStreamForReadAsync("sample.txt");
+
+                using (var reader = new StreamReader(file))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+
+                        var l = line.Split(",");
+                        if (l.Length > 6)
+                        {
+                            var img = "";
+                            if (File.Exists(ApplicationData.Current.LocalFolder.Path + "\\" + l[7]))
+                            {
+                                img = Path.Combine(ApplicationData.Current.LocalFolder.Path, l[7]);
+                            }
+                            else
+                            {
+                                img = "Images/wood.jpg";
+                            }
+                            Collection.Add(new Items() { ITEMCODE = l[0], ITEMNAME = l[1], DEPT = l[2], PURCHASEPRICE = int.Parse(l[3]), SALESPRICE = int.Parse(l[4]), QTY = int.Parse(l[5]), EXPIREDATE = l[6], IMAGE = img });
+                        }
+                    }
+                    reader.Close();
+                }
+
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+        public ObservableCollection<Items> Activities { get; set; }
+        List<Items> collection = new List<Items>();
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+
+        {
+           //GetRecords();
+            BlankPage5 messageData = new BlankPage5();
+
+            ItemListView.ItemsSource = messageData.Collection;
+
+            ItemListView.SelectedIndex = 0;
+
+        }
+
+        public List<Items> Collection
+
+        {
+
+            get
+
+            {
+
+                return this.collection;
+
+            }
+
+        }
+    }
+    public class Item
+
+    {
+
+        private string _Title;
+
+        public string Title
+
+        {
+
+            get
+
+            {
+
+                return this._Title;
+
+            }
+
+            set
+
+            {
+
+                this._Title = value;
+
+            }
+
+        }
+
+        private string _Price;
+
+        public string Price
+
+        {
+
+            get
+
+            {
+
+                return this._Price;
+
+            }
+
+            set
+
+            {
+
+                this._Price = value;
+
+            }
+
+        }
+
+        private ImageSource _Image;
+
+        public ImageSource Image
+
+        {
+
+            get
+
+            {
+
+                return this._Image;
+
+            }
+
+            set
+
+            {
+
+                this._Image = value;
+
+            }
+
+        }
+
+        public void SetImage(Uri baseUri, String path)
+
+        {
+
+            Image = new BitmapImage(new Uri(baseUri, path));
+
+        }
+
+        private string _Link;
+
+        public string Link
+
+        {
+
+            get
+
+            {
+
+                return this._Link;
+
+            }
+
+            set
+
+            {
+
+                this._Link = value;
+
+            }
+
+        }
+
+        private string _Category;
+
+        public string Category
+
+        {
+
+            get
+
+            {
+
+                return this._Category;
+
+            }
+
+            set
+
+            {
+
+                this._Category = value;
+
+            }
+
+        }
+
+        private string _Description;
+
+        public string Description
+
+        {
+
+            get
+
+            {
+
+                return this._Description;
+
+            }
+
+            set
+
+            {
+
+                this._Description = value;
+
+            }
+
+        }
+
+        private string _Content;
+
+        public string Content
+
+        {
+
+            get
+
+            {
+
+                return this._Content;
+
+            }
+
+            set
+
+            {
+
+                this._Content = value;
+
+            }
+
+        }
+
+    }
+
+
+}
---
+++ Login.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MonthlyProject
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Login : Page
+    {
+        public Login()
+        {
+            this.InitializeComponent();
+            StorageFolder appFolder = ApplicationData.Current.LocalFolder;
+            loc2.Text = "Path: " + appFolder.Path;
+            loc1.Text = "Path: " + appFolder.Path;
+            CreateTextFile2();
+
+
+
+
+        }
+        private void login(object sender, RoutedEventArgs e)
+        {
+            bool isInLandscapeMode =
+ Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().Orientation ==
+ Windows.UI.ViewManagement.ApplicationViewOrientation.Landscape;
+            if(!isInLandscapeMode)
+            GetRecords();
+            else
+               GetRecordLanscapes();
+        }
+        public async Task GetRecords()
+        {
+            var all = "";
+            var folder = ApplicationData.Current.LocalFolder;
+
+            try
+            {
+                var file = await folder.OpenStreamForReadAsync("users.txt");
+
+                using (var reader = new StreamReader(file))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+
+                        var l = line.Split(",");
+                        if (l.Length > 1)
+                        {
+                            if(l[0]==user.Text && l[1] == pass.Password)
+                            {
+                                this.Frame.Navigate(typeof(MainPage));
+                                return;
+                            }
+
+                        }
+                    }
+                    error1.Text = "Invalid User Name or Password. Please try again .....";
+                }   
+
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+        public async Task GetRecordLanscapes()
+        {
+            var all = "";
+            var folder = ApplicationData.Current.LocalFolder;
+
+            try
+            {
+                var file = await folder.OpenStreamForReadAsync("users.txt");
+
+                using (var reader = new StreamReader(file))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+
+                        var l = line.Split(",");
+                        if (l.Length > 1)
+                        {
+                            if (l[0] == tuser.Text && l[1] == tpass.Password)
+                            {
+                                this.Frame.Navigate(typeof(MainPage));
+                                return;
+                            }
+
+                        }
+                    }
+                    error2.Text = "Invalid User Name or Password. Please try again .....";
+                }
+
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+        private void CreateTextFile2()
+        {
+            CreateTextFile();
+        }
+        private async Task CreateTextFile()
+        {
+
+            Windows.Storage.StorageFolder storageFolder =
+    Windows.Storage.ApplicationData.Current.LocalFolder;
+            if (!File.Exists(storageFolder.Path + "\\sample.txt"))
+            {
+                Windows.Storage.StorageFile sampleFile = await storageFolder.CreateFileAsync("sample.txt", Windows.Storage.CreationCollisionOption.OpenIfExists);
+                string s = "I-003,Meril(M),Meril,20,22,1000,2022-01-02,WOOD.JPG" + Environment.NewLine +
+    "F-002,My Salmon, Fish,122,150,213,2022-01-01,WOOD.JPG";
+
+                await Windows.Storage.FileIO.AppendTextAsync(sampleFile, s + Environment.NewLine);
+            }
+            if (!File.Exists(storageFolder.Path + "\\users.txt"))
+            {
+                Windows.Storage.StorageFile sampleFile2 = await storageFolder.CreateFileAsync("users.txt", Windows.Storage.CreationCollisionOption.OpenIfExists);
+                var s = "karim,123" + Environment.NewLine +
+    "rahim,456";
+
+                await Windows.Storage.FileIO.AppendTextAsync(sampleFile2, s + Environment.NewLine);
+            }
+        }
+
+        private void tpass_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void error2_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
